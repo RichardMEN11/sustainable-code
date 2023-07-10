b913357de2ba6b325cb755e6ae2bf69343007ebc
RichardMEN11 <richardmenning@googlemail.com>
2023-07-10 19:23:04 +0200

    feat: :sparkles: add react app to display values

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "sustainable-code" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'sustainable-code.helloWorld',
     () => {
@@ -25,10 +18,47 @@ export async function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(disposable);
 
-  const mainWindow = vscode.window.createOutputChannel('main');
+  // Create a webview panel
+  const panel = vscode.window.createWebviewPanel(
+    'sustainableCode',
+    'Sustainable Code',
+    vscode.ViewColumn.One,
+    {
+      enableScripts: true,
+    }
+  );
 
-  mainWindow.appendLine('test');
+  // Get the path to the bundled React HTML file
+  const bundlePath = path.join(
+    context.extensionPath,
+    'src/view/dist',
+    'index.html'
+  );
+
+  const htmlContent = fs.readFileSync(bundlePath, 'utf8');
+
+  panel.webview.html = htmlContent;
+
+  console.log(panel.webview.html);
+
+  panel.reveal(vscode.ViewColumn.One);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(bundlePath: vscode.Uri) {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>My Extension</title>
+    </head>
+    <body>
+      <iframe src="${bundlePath}" frameborder="0"></iframe>
+    </body>
+    </html>
+  `;
+}
